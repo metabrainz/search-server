97797978478c3f2585cafa0562c8dcc8b8b8b5b0
Aurélien Mino <aurelien.mino@gmail.com>
2008-10-26 14:58:36 +0000

    Show label code in search results

--- lib/labelsearch.py
+++ lib/labelsearch.py
@@ -56,7 +56,7 @@ class LabelSearch(search.TextSearch):
    def __init__(self, index):
        search.TextSearch.__init__(self, index)
        self.setDefaultField('label')
-       self.setPrefixes(('label', 'sortname', 'alias', 'begin', 'end', 'type', 'laid', 'comment'))
+       self.setPrefixes(('label', 'sortname', 'alias', 'begin', 'end', 'type', 'laid', 'comment', 'code'))
 
    def mangleQuery(self, query):
        query = re.sub("type:(\d)", replaceType, query)
@@ -70,7 +70,7 @@ class LabelSearch(search.TextSearch):
        rel = self.rel
        
        out = u'<div><table class="searchresults">'
-       out += u'<tr class="searchresultsheader"><td>Score</td><td>Label</td><td>Sortname</td><td>Type</td><td>Begin</td><td>End</td>'
+       out += u'<tr class="searchresultsheader"><td>Score</td><td>Label</td><td>Sortname</td><td>Code</td><td>Type</td><td>Begin</td><td>End</td>'
        if rel: out += u'<td>Rel</td>'
        out += u'</tr>'
        
@@ -78,6 +78,7 @@ class LabelSearch(search.TextSearch):
            label = doc.get('label') or u''
            sortname = doc.get('sortname') or u''
            comment = doc.get('comment') or u''
+           code = doc.get('code') or u''
            begin = doc.get('begin') or u''
            end = doc.get('end') or u''
            laid = doc.get('laid') or u''
@@ -89,6 +90,7 @@ class LabelSearch(search.TextSearch):
                    (self.escape(laid), self.escape(label))
            if comment: out += " (%s)" % self.escape(comment)
            out += u"</td><td>%s</td>" % self.escape(sortname)
+           out += u"<td>%s</td>" % self.escape(code)
            out += u"<td>%s</td>" % self.escape(type)
            out += u"<td>%s</td><td>%s</td>" % (self.escape(begin), self.escape(end))
            if rel: out += u"<td><a href=\"/show/artist/relationships.html?labelid=%s&amp;addrel=1\">rel</a></td>" % self.escape(laid)
@@ -106,6 +108,7 @@ class LabelSearch(search.TextSearch):
            label = doc.get('label') or u''
            sortname = doc.get('sortname') or u''
            type = doc.get('type') or u''
+           code = doc.get('code') or u''
            try:
                type = TYPE_MAPPING[type]
            except KeyError:
@@ -121,6 +124,8 @@ class LabelSearch(search.TextSearch):
            out += u'><name>%s</name>' % self.escape(label)
            if sortname:
                out += u"<sort-name>%s</sort-name>" % self.escape(sortname)
+           if code:
+               out += u"<label-code>%s</label-code>" % self.escape(code)
            if begin or end:
                out += u'<life-span'
                if begin: out += u' begin="%s"' % self.escape(begin)
