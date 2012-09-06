ecb12cdb8edac2f498e796bd4046855acb30a8ba
Aurélien Mino <aurelien.mino@gmail.com>
2012-09-06 21:49:43 +0000

    Improved helper script that helps identify tables used for indexing.

--- updater/src/main/bin/find-index-deps.sh
+++ updater/src/main/bin/find-index-deps.sh
@@ -1,7 +1,12 @@
 BASE=../../../../search-server-index/src/main/java/org/musicbrainz/search/index
-grep -E "(FROM|JOIN)+" $BASE/$1Index.java | sed -e 's/^.*\(FROM\|JOIN\) \([a-z_]*\)$/\2/' #| sort -u
-grep -E "(FROM|JOIN)+" $BASE/$1Index.java | sed -e 's/^.*\(FROM\|JOIN\) \([a-z_]*\).*$/\2/' | sort | uniq -c \
-	| grep -vE '(artist_name|label_name|track_name|release_name|work_name)' \
-	| grep -vE '(track_raw|release_raw|cdtoc_raw|replication_control)' \
-	| grep -vE '(artist_type|label_type|country|gender|language|script|medium_format|release_group_type|work_type|release_status)' \
-	| grep -vE '(artist_credit|replication_control)'
+ENTITY=$1
+grep -E "(FROM|JOIN)+" $BASE/${ENTITY}Index.java | sed -e 's/^.*\(FROM\|JOIN\)\s\+\([a-z_]*\)$/\2/' #| sort -u
+grep -E "(FROM|JOIN)+" $BASE/${ENTITY}Index.java | sed -e 's/^.*\(FROM\|JOIN\)\s\+\([a-z_]*\).*$/\2/' | sort | uniq -c \
+	| grep -viE "\b${ENTITY}\b" \
+	| grep -viE '\b(artist_name|label_name|track_name|release_name|work_name)\b' \
+	| grep -viE '\b(track_raw|release_raw|cdtoc_raw)\b' \
+	| grep -viE '\b(artist_type|label_type|release_group_primary_type|release_group_secondary_type|work_type)\b' \
+	| grep -viE '\b(country|gender|language|script|medium_format|release_status)\b' \
+	| grep -viE '\b(link|link_type)\b' \
+	| grep -viE '\b(puid|tag)\b' \
+	| grep -viE '\b(replication_control)\b'
