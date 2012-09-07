8a7cc5870d53dbdaa36446b113b31dfabd91ee9a
Aurélien Mino <aurelien.mino@gmail.com>
2012-09-07 22:17:19 +0000

    Refactoring of LiveDataFeedIndexUpdater so it loads replication packets only once, and not for each index type.

--- index/src/main/java/org/musicbrainz/search/index/ReplicationInformation.java
+++ index/src/main/java/org/musicbrainz/search/index/ReplicationInformation.java
@@ -1,9 +1,39 @@
 package org.musicbrainz.search.index;
 
-public class ReplicationInformation {
+public class ReplicationInformation implements Comparable<ReplicationInformation> {
 
 	public Integer schemaSequence;
 	public Integer replicationSequence;
 	public Integer changeSequence;
+	
+	@Override
+	public int compareTo(ReplicationInformation other) {
+		int result;
 		
+		// SchemaSequence
+		result = this.schemaSequence.compareTo(other.schemaSequence);
+		if (result != 0) {
+			return result;
+		}
+		
+		// ReplicationSequence
+		result = this.replicationSequence.compareTo(other.replicationSequence);
+		if (result != 0) {
+			return result;
+		}
+		
+		// ReplicationSequence (might be null)
+		if (this.changeSequence == null && other.changeSequence == null) {
+			result = 0;
+		} else if (this.changeSequence == null) {
+			result = -1;
+		} else if (other.changeSequence == null) {
+			result = 1;
+		} else {
+			result = this.changeSequence.compareTo(other.changeSequence);
+		}
+		return result;
+	}
+
+	
 }
--- updater/src/main/java/org/musicbrainz/search/update/LiveDataFeedIndexUpdater.java
+++ updater/src/main/java/org/musicbrainz/search/update/LiveDataFeedIndexUpdater.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
@@ -15,6 +19,7 @@ import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 import org.apache.commons.lang.time.StopWatch;
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -49,8 +54,15 @@ public class LiveDataFeedIndexUpdater {
 	
 	private Connection mainDbConn;
 	
+	private LiveDataFeedIndexUpdaterOptions options;
+	private List<DatabaseIndex> indexes;
+	private Map<DatabaseIndex,IndexWriter> indexWriters =  new HashMap<DatabaseIndex,IndexWriter>();
+	private Map<DatabaseIndex,ReplicationInformation> indexReplicationInfos = new HashMap<DatabaseIndex,ReplicationInformation>();
+	private Map<DatabaseIndex,ChangesAnalyzer> indexChangesAnalyzers = new HashMap<DatabaseIndex,ChangesAnalyzer>();
+	
 	public LiveDataFeedIndexUpdater(LiveDataFeedIndexUpdaterOptions options) {
 		
+		this.options = options;
 		LOGGER.setUseParentHandlers(false);
 	    Handler conHdlr = new ConsoleHandler();
 	    conHdlr.setFormatter(new Formatter() {
@@ -60,69 +72,166 @@ public class LiveDataFeedIndexUpdater {
 	      }
 	    });
 	    LOGGER.addHandler(conHdlr);
-	    Level logLevel = options.isVerbose() ? Level.FINE : Level.INFO;
+	    Level logLevel = this.options.isVerbose() ? Level.FINE : Level.INFO;
 	    LOGGER.setLevel(logLevel);
 	    conHdlr.setLevel(logLevel);
 	    
 //	    dependencyTrees = initDependencyTrees();
 	}
 	
-	public void update() throws SQLException, IOException {
-		
-		LiveDataFeedIndexUpdaterOptions options = LiveDataFeedIndexUpdaterOptions.getInstance();
+	public void init() throws SQLException, IOException {
 		
         // Connect to main database
-        mainDbConn = options.getMainDatabaseConnection();
+        mainDbConn = this.options.getMainDatabaseConnection();
     
-        StopWatch clock = new StopWatch();
-
         // MusicBrainz data indexing
-        DatabaseIndex[] indices = {
-                new ArtistIndex(mainDbConn),
-                new ReleaseIndex(mainDbConn),
-                new ReleaseGroupIndex(mainDbConn),
-                new RecordingIndex(mainDbConn),
-                new LabelIndex(mainDbConn),
-                new WorkIndex(mainDbConn),
-                new AnnotationIndex(mainDbConn),
-                new TagIndex(mainDbConn),
-        };
+        indexes = new ArrayList<DatabaseIndex>() {{
+            add(new ArtistIndex(mainDbConn));
+            add(new ReleaseIndex(mainDbConn));
+            add(new ReleaseGroupIndex(mainDbConn));
+            add(new RecordingIndex(mainDbConn));
+            add(new LabelIndex(mainDbConn));
+            add(new WorkIndex(mainDbConn));
+            add(new AnnotationIndex(mainDbConn));
+            add(new TagIndex(mainDbConn));
+        }};
 
-        List<String> indexesToBeBuilt = new ArrayList<String>();
-        for (DatabaseIndex index : indices) {
-            // Check if this index should be built
-            if (options.buildIndex(index.getName())) {
-                indexesToBeBuilt.add(index.getName());
-            }
-        }
-        // Create temporary tables used by multiple indexes
-        CommonTables commonTables = new CommonTables(mainDbConn, indexesToBeBuilt);
-        commonTables.createTemporaryTables(true);
-
-        for (DatabaseIndex index : indices) {
-
-            // Check if this index should be built
+        // Remove from the indexes list indexes that should not be handled in this run
+        Iterator<DatabaseIndex> it = indexes.iterator();
+        while (it.hasNext()) {
+        	DatabaseIndex index = it.next();
             if (!options.buildIndex(index.getName())) {
-            	LOGGER.info("");
-            	LOGGER.info("Skipping index: " + index.getName());
-                continue;
+            	it.remove();
             }
+		}
+        
+        // Step 1: Initialize index writers and load replication info of each index 
+        for (DatabaseIndex index : indexes) {
 
-            clock.start();
-            LOGGER.info("");
+            // Initialize index writer
+            IndexWriter indexWriter = createWriterForIndex(index, options);
+            indexWriters.put(index, indexWriter);
+            
+            // Load replication information
+            IndexReader indexReader = IndexReader.open(indexWriter, true);
+            ReplicationInformation replicationInfo = index.readReplicationInformationFromIndex(indexReader);
+            indexReplicationInfos.put(index, replicationInfo);
+            
+            // Initialize the changes analyzer
+            DatabaseIndexDependencies dependencies = new DatabaseIndexDependencies(index.getName());
+            try {
+    			dependencies.loadFromConfigFile( getClass().getResourceAsStream("/dependencies.xml") );
+    		} catch (JDOMException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+            ChangesAnalyzer changesAnalyzer = new ChangesAnalyzer(index, dependencies);
+            indexChangesAnalyzers.put(index, changesAnalyzer);
+        }
+	}
+	
+	public void destroy() throws CorruptIndexException, IOException {
+        for(DatabaseIndex index : indexes) {
+        	indexWriters.get(index).close();
+        }
+	}
+	
+	public void update() throws SQLException, IOException {
+		
+        StopWatch clock = new StopWatch();
+        
+        if (indexes.size() == 0) {
+        	LOGGER.info("No selected indexes to update, aborting");
+        	return;
+        }
+               
+        // Step 1: Load all replication packets and analyze changes to determine what need to be reindexed
+        List<ReplicationInformation> list = new ArrayList<ReplicationInformation>(indexReplicationInfos.values());
+        Collections.sort(list);
+        ReplicationInformation earliestReplicationInformation = list.get(0);
+        
+        Integer lastChangeSequence = earliestReplicationInformation.changeSequence;
+        ReplicationPacketIterator itPacket = new ReplicationPacketIterator(earliestReplicationInformation);
+        while (itPacket.hasNext()) {
+        	
+        	ReplicationPacket packet = itPacket.next();           	
+        	LOGGER.info("Loading packet #" + packet.getReplicationSequence());
+        	
+        	for (DatabaseIndex index : indexes) {
+
+        		// SchemaSequence of packet should match the packet's one 
+            	if (indexReplicationInfos.get(index).schemaSequence != packet.getSchemaSequence()) {
+            		LOGGER.fine("Ignoring packet #" + packet.getReplicationSequence() + " for index " + index.getName() + ": SCHEMA sequence mismatches");
+            	// Check if this packet hasn't already been processed for this index (by comparing replication information)
+            	} else if (indexReplicationInfos.get(index).compareTo(itPacket.getCurrentReplicationPosition()) > 0) {
+            		LOGGER.fine("Ignoring packet #" + packet.getReplicationSequence() + " for index " + index.getName() + ": already indexed");
+        		// Otherwise process the packet to find changes
+            	} else {
+            		LOGGER.fine("Analyzing packet #" + packet.getReplicationSequence() + " for index " + index.getName());
+            		indexChangesAnalyzers.get(index).analyze(packet, lastChangeSequence);	
+            	}
+        	}
+        	
+        	lastChangeSequence = packet.getMaxChangeId();
+        }
+
+        // Step 2: Create temporary tables, used by multiple indexes, if changes has been found
+        boolean changesExist = false;
+        for(ChangesAnalyzer changesAnalyzer : indexChangesAnalyzers.values()) {
+        	if (changesAnalyzer.hasChanges()) {
+        		changesExist = true;
+        		break;
+        	}
+        }
+        if (changesExist) {
+            List<String> indexesToBeBuilt = new ArrayList<String>();
+            for(DatabaseIndex index : indexes) {
+            	indexesToBeBuilt.add(index.getName());
+            }
+	        CommonTables commonTables = new CommonTables(mainDbConn, indexesToBeBuilt);
+	        commonTables.createTemporaryTables(true);
+        } 
+        
+        // Step 3: Update the existing indexes from found changes
+        for (DatabaseIndex index : indexes) {
+
+        	ChangesAnalyzer changesAnalyzer = indexChangesAnalyzers.get(index);
+        	if (!changesAnalyzer.hasChanges()) {
+        		LOGGER.info("No changes found for index: " + index.getName());
+        		continue;
+        	}
+        	
+        	clock.start();
             LOGGER.info("Started updating index: " + index.getName());
             
+            IndexWriter indexWriter = indexWriters.get(index);
+            ReplicationInformation replicationInfo = indexReplicationInfos.get(index);
+            
             try {
-				updateDatabaseIndex(index, options);
+				updateDatabaseIndex(index, indexWriter, replicationInfo, changesAnalyzer, itPacket.getCurrentReplicationPosition());
 			} catch (DatabaseSchemaChangedException e) {
 				LOGGER.severe("DatabaseSchemaChangedException: " +  e.getMessage());
 			}
             
+            changesAnalyzer.reset();
+            
             clock.stop();
-            LOGGER.info("Finished updating index: " + index.getName() + " in " + Float.toString(clock.getTime()/1000) + " seconds");
+            LOGGER.fine("Finished updating index: " + index.getName() + " in " + Float.toString(clock.getTime()/1000) + " seconds");
             clock.reset();
         }
 
+	}
+	
+	private IndexWriter createWriterForIndex(DatabaseIndex index, LiveDataFeedIndexUpdaterOptions options) throws IOException {
+	       IndexWriterConfig writerConfig = new IndexWriterConfig(LuceneVersion.LUCENE_VERSION, index.getAnalyzer());
+
+	       String path = options.getIndexesDir() + index.getFilename();
+	       IndexWriter indexWriter = new ThreadedIndexWriter(FSDirectory.open(new File(path)),
+	                writerConfig,
+	                Runtime.getRuntime().availableProcessors(),
+	                10);
+	       
+	       return indexWriter;
 	}
 	
 	/**
@@ -133,54 +242,13 @@ public class LiveDataFeedIndexUpdater {
      * @throws SQLException 
 	 * @throws DatabaseSchemaChangedException 
      */
-    private void updateDatabaseIndex(DatabaseIndex index, LiveDataFeedIndexUpdaterOptions options) throws IOException, SQLException, DatabaseSchemaChangedException
+    private void updateDatabaseIndex(DatabaseIndex index, IndexWriter indexWriter, ReplicationInformation currentReplicationInfo, ChangesAnalyzer changesAnalyzer, ReplicationInformation newReplicationInfo) throws IOException, SQLException, DatabaseSchemaChangedException
     {
 
-        IndexWriterConfig writerConfig = new IndexWriterConfig(LuceneVersion.LUCENE_VERSION,index.getAnalyzer());
-
-    	String path = options.getIndexesDir() + index.getFilename();
-        IndexWriter indexWriter = new ThreadedIndexWriter(FSDirectory.open(new File(path)),
-                writerConfig,
-                Runtime.getRuntime().availableProcessors(),
-                10);
-
-        IndexReader reader = indexWriter.getReader();     
-        ReplicationInformation replicationInfo = index.readReplicationInformationFromIndex(reader);
-        DatabaseIndexDependencies dependencies = new DatabaseIndexDependencies(index.getName());
-        try {
-			dependencies.loadFromConfigFile( getClass().getResourceAsStream("/dependencies.xml") );
-		} catch (JDOMException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        LOGGER.info("Current index properties: schema_sequence=" + currentReplicationInfo.schemaSequence +
+        		", replication_sequence=" + currentReplicationInfo.replicationSequence + 
+        		", change_sequence=" + (currentReplicationInfo.changeSequence != null ? currentReplicationInfo.changeSequence : "") );
         
-        Integer lastChangeSequence = replicationInfo.changeSequence;
-
-        LOGGER.info("Current index properties: schema_sequence=" + replicationInfo.schemaSequence +
-        		", replication_sequence=" + replicationInfo.replicationSequence + 
-        		", change_sequence=" + (replicationInfo.changeSequence != null ? replicationInfo.changeSequence : "") );
-        
-        // Load and process each replication packet released since
-        ChangesAnalyzer changesAnalyzer = new ChangesAnalyzer(index, dependencies);
-        
-        boolean packetsFound = false;
-        ReplicationPacketIterator itPacket = new ReplicationPacketIterator(replicationInfo);
-        while (itPacket.hasNext()) {
-        	
-        	packetsFound = true;
-        	ReplicationPacket packet = itPacket.next();           	
-        	LOGGER.info("Loading packet #" + packet.getReplicationSequence());
-        	
-        	if (replicationInfo.schemaSequence != packet.getSchemaSequence()) {
-        		LOGGER.info("Aborting, new packet is for a different SCHEMA sequence");
-        		throw new DatabaseSchemaChangedException(replicationInfo.schemaSequence, packet.getSchemaSequence());
-        	}
-        	
-        	changesAnalyzer.analyze(packet, lastChangeSequence);
-        	
-        	lastChangeSequence = packet.getMaxChangeId();
-        }
-
         // We're done parsing all replication packets and analyzing impacted entities
 		Term term = new Term(MetaIndexField.META.getName(), MetaIndexField.META_VALUE);
 		TermQuery query = new TermQuery(term);
@@ -207,9 +275,10 @@ public class LiveDataFeedIndexUpdater {
         	index.destroy();
     	}
     	
-    	// Only update the index if we've been able to load at least one packet
-    	if (packetsFound) {
-    		index.updateMetaInformation(indexWriter, itPacket.getCurrentReplicationPosition());
+    	// Only update the index if we've processed some database changes
+    	if (currentReplicationInfo.compareTo(newReplicationInfo) != 0) {
+    		
+    		index.updateMetaInformation(indexWriter, newReplicationInfo);
     		indexWriter.commit();
     		// TODO: index don't need to be optimized on each update, it's way too resource intensive
     		// => disabled for now, need to be done on a regular basis that should determined
@@ -217,8 +286,8 @@ public class LiveDataFeedIndexUpdater {
         
             // Check to we have as much Lucene documents as Database rows
             int dbRows = index.getNoOfRows(Integer.MAX_VALUE);
-            reader = reader.reopen();
-            LOGGER.info(dbRows + " rows in database, " + (reader.maxDoc()-1) + " lucene documents");
+            IndexReader indexReader = IndexReader.open(indexWriter, true);
+            LOGGER.info(dbRows + " rows in database, " + (indexReader.maxDoc()-1) + " lucene documents");
     	} else {
     		LOGGER.info("No changes found");
     	}
@@ -260,7 +329,9 @@ public class LiveDataFeedIndexUpdater {
         }
         
         LiveDataFeedIndexUpdater updater = new LiveDataFeedIndexUpdater(options);
+        updater.init();
         updater.update();
+        updater.destroy();
 	}
     
 }
