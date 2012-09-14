8a0e6f456c6a255b8bc7da76bd87cca38c9b75f8
ijabz <ijabz@fastmail.fm>
2012-09-14 09:29:29 +0000

    #SEARCH-226:fixed work output

--- servlet/src/main/java/org/musicbrainz/search/servlet/moxy/WorkAdapter.java
+++ servlet/src/main/java/org/musicbrainz/search/servlet/moxy/WorkAdapter.java
@@ -10,30 +10,49 @@ import java.util.List;
 
 public class WorkAdapter extends XmlAdapter<WorkAdapter.AdaptedWork, Work> {
 
-    public static class AdaptedWork {
+    public static class AdaptedWork extends Work {
         public List<Relation> relations = new ArrayList<Relation>();
     }
 
+    /**
+     * Call when convert model to json, replaces work in model with adaptedWork
+     * which does not contain a list of RelationList, instead all relations in each existing
+     * RelationList are merged into a list of relations. We do this because it is not possible to merge
+     * a List of RelationLists into the work using oxml.xml mapping
+     */
     @Override
     public AdaptedWork marshal(Work work) throws Exception {
+        
         AdaptedWork adaptedWork = new AdaptedWork();
         for(RelationList relationList : work.getRelationList()) {
             for(Relation relation : relationList.getRelation()) {
                 adaptedWork.relations.add(relation);
             }
         }
+
+        //Also need to copy any other elements/attributes we may want to output
+        adaptedWork.setAliasList(work.getAliasList());
+        adaptedWork.setArtistCredit(work.getArtistCredit());
+        adaptedWork.setDisambiguation(work.getDisambiguation());
+        adaptedWork.setId(work.getId());
+        adaptedWork.setIswcList(work.getIswcList());
+        adaptedWork.setLanguage(work.getLanguage());
+        adaptedWork.setRating(work.getRating());
+        adaptedWork.setScore(work.getScore());
+        adaptedWork.setTitle(work.getTitle());
+        adaptedWork.setTagList(work.getTagList());
+        adaptedWork.setType(work.getType());
+        adaptedWork.setUserRating(work.getUserRating());
+        adaptedWork.setUserTagList(work.getUserTagList());
         return adaptedWork;
     }
 
+    /*
+    Not used in Search Server
+     */
     @Override
     public Work unmarshal(AdaptedWork adaptedWork) throws Exception {
-        Work work = new Work();
-        RelationList relationList = new RelationList();
-        for(Relation relation : adaptedWork.relations) {
-            relationList.getRelation().add(relation);
-        }
-        work.getRelationList().add(relationList);
-        return work;
+        throw new UnsupportedOperationException("Umarshalling json back to model not supported");
     }
 
 }
