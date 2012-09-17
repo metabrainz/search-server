e34dee7ef3744e3cde11ea6bcfa9b088d4cbacea
Aurélien Mino <aurelien.mino@gmail.com>
2012-09-17 21:33:24 +0000

    Some documentation about the updater module.

--- updater/updateindex.sh
+++ updater/updateindex.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+############################################################
+#                       SETTINGS                           #
+############################################################
+
+# Local slave database settings
 DB_HOST=localhost
 DB_NAME=musicbrainz
 DB_USER=musicbrainz
 DB_PASSWORD=musicbrainz
-INDEXES_DIR=../index/data
-INDEXES=artist,releasegroup,label,tag,annotation,work
+
+# Location of search indexes
+INDEXES_DIR=/home/search/indexdata
+
+# URL of the search server that need to be notified once indexes has been updated
 #SERVLET_HOST=localhost:8080
 
-SEARCH_UPDATER_JAR=target/updater-2.0-SNAPSHOT-jar-with-dependencies.jar
+# Indexes that will be updated
+INDEXES=artist,releasegroup,label,tag,annotation,work,release,recording
+
+############################################################
+#                       SCRIPT                             #
+############################################################
+
+SEARCH_UPDATER_JAR=`dirname "$0"`/target/updater-2.0-SNAPSHOT-jar-with-dependencies.jar
 LOCK_FILE=/tmp/.mb-search-updater.lock
 
 if [ -e $LOCK_FILE ]; then
