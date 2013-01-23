41ae9ea5a954d08aa99c0a2177ba4e65758d298f
Aurélien Mino <aurelien.mino@gmail.com>
2013-01-23 21:50:28 +0000

    SEARCH-255: Better handling of options/config params in updateindex.sh

--- updater/updateindex.sh
+++ updater/updateindex.sh
@@ -34,9 +34,9 @@ touch $LOCK_FILE
 
 # Run the updater: if $INDEXES is set, use for specifying indexes to update
 if [ -z "$INDEXES" ]; then
-    java -Xmx512M -jar $SEARCH_UPDATER_JAR --db-host $DB_HOST --db-name $DB_NAME --db-user $DB_USER --db-password $DB_PASSWORD --indexes-dir $INDEXES_DIR "$@"
+    java -Xmx512M -jar $SEARCH_UPDATER_JAR --db-host $DB_HOST --db-name $DB_NAME --db-user $DB_USER --db-password "$DB_PASSWORD" --indexes-dir $INDEXES_DIR $@
 else
-    java -Xmx512M -jar $SEARCH_UPDATER_JAR --indexes $INDEXES --db-host $DB_HOST --db-name $DB_NAME --db-user $DB_USER --db-password $DB_PASSWORD --indexes-dir $INDEXES_DIR "$@"
+    java -Xmx512M -jar $SEARCH_UPDATER_JAR --indexes $INDEXES --db-host $DB_HOST --db-name $DB_NAME --db-user $DB_USER --db-password "$DB_PASSWORD" --indexes-dir $INDEXES_DIR $@
 fi
 
 # Notify the search servlet that indexes have changed
