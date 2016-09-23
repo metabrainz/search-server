5a4d12f1f34f22cabb6d1b6dcacae05364bf1988
Robert Kaye <rob@musicbrainz.org>
2016-09-23 23:08:42 +0200

    First cut at getting an indexer working. Not working yet -- interim check-in.

---
+++ bin/build-indexes.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This script expects the following evn vars to be set
+#      POSTGRES_HOST
+#      POSTGRES_PORT
+#      POSTGRES_DB
+#      POSTGRES_USER
+#      POSTGRES_PASSWD
+
+SEARCH_HOME=/home/search
+SEARCH_JAVA_OPTS_INDEXER="-Xmx512M"
+JAR=/home/search/index.jar
+
+echo $JAVA_HOME/bin/java \
+	$SEARCH_JAVA_OPTS_INDEXER \
+	-jar "$JAR" \
+	--db-host "$POSTGRES_HOST" \
+	--db-name "$POSTGRES_DB" \
+	--db-user "$POSTGRES_USER" \
+	--db-password "$POSTGRES_PASSWD" \
+	--indexes "$@"
+exec $JAVA_HOME/bin/java \
+	$SEARCH_JAVA_OPTS_INDEXER \
+	-jar "$JAR" \
+	--db-host "$POSTGRES_HOST" \
+	--db-name "$POSTGRES_DB" \
+	--db-user "$POSTGRES_USER" \
+	--db-password "$POSTGRES_PASSWD" \
+	--indexes "$@"
