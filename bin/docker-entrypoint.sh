843b0046d024d299bd5ffab69e66c32936dc541f
Robert Kaye <rob@metabrainz.org>
2016-09-24 15:34:50 +0000

    First complete indexes being rotated...

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -12,16 +12,18 @@ fi
 IN_PROG_DIR=$SEARCH_HOME/data/in-progress
 INDEXES_DIR=$SEARCH_HOME/data
 
-#rsync --config=/etc/rsyncd.conf --daemon
-rsync --log-file=/dev/stdout --config=/etc/rsyncd.conf --daemon
+rsync --config=/etc/rsyncd.conf --daemon
 
 while [ 1 ] 
 do
 
     mkdir -p $IN_PROG_DIR
     cd $IN_PROG_DIR
-    $SEARCH_HOME/bin/build-indexes.sh url,area,release,editor
+    $SEARCH_HOME/bin/build-indexes.sh area,artist,cdstub,instrument,label,place,editor,event,release,releasegroup,cdstub,annotation,series,work,tag,url
     cd $SEARCH_HOME
-    $SEARCH_HOME/bin/smart-rotate.py $INDEXES_VERSION $IN_PROG_DIR $INDEXES_DIR
+    $SEARCH_HOME/bin/smart-rotate.py $INDEXES_VERSION $IN_PROG_DIR/data $INDEXES_DIR
 
 done
+
+#recording,
+#freedb,
--- bin/watch-for-reload.sh
+++ bin/watch-for-reload.sh
@@ -4,45 +4,54 @@ WATCH_FILE=${@:1:1}
 PROG=${@:2:1}
 CURRENT_TS=0
 
-# Start the main program
-cd $JETTY_HOME
-echo $PROG "${@:3}" 
-$PROG "${@:3}" &
-
-
 while [ 1 ]
 do
     rsync rsync://search@indexer:/data/$INDEXES_VERSION/ --list-only > /tmp/data-list
     if [ $? != "0" ]
     then
-	echo "=============== Failed to get list of data sets. sleeping"
+	echo "== Failed to get list of data sets. sleeping"
 	sleep 5
 	continue
     fi
     TS=`cat /tmp/data-list | colrm 1 46 | grep -i . | sort -r | head -1`
-    echo "timestamp $TS current $CURRENT_TS"
     rm /tmp/data-list
 
     if [ "$CURRENT_TS" == "$TS" ]
     then
-	echo "=============== No new data available."
         sleep 10
         continue
     fi
 
     # Sync over the indexes
-    mkdir -p $HOME_SEARCH/data/$TS
-    rsync -rv rsync://search@indexer:/data/$INDEXES_VERSION/$TS $HOME_SEARCH/data/$TS
+    mkdir -p $SEARCH_HOME/data/$TS
+    rsync -rv rsync://search@indexer:/data/$INDEXES_VERSION/$TS $SEARCH_HOME/data
     if [ $? != "0" ]
     then
-	echo "=============== Failure during sync of dataset. Starting over again."
+	echo "== Failure during sync of dataset. Starting over again."
 	sleep 5
 	continue
     fi
 
+    # remove the old data set and move the current one into place
+    rm -rf $SEARCH_HOME/data/current
+    mv $SEARCH_HOME/data/$TS $SEARCH_HOME/data/current
+    ln -fs $SEARCH_HOME/data/current $SEARCH_HOME/indexdata
+
     CURRENT_TS=$TS
 
+    # this has got to be my favorite command EVAR
+    echo "Kill the search server... \ø/"
+    killall -9 java
+  
+    # Start the search server again
+    cd $JETTY_HOME
+    echo $PROG "${@:3}" 
+    $PROG "${@:3}" &
+
+    # take a well deserved break!
     sleep 3
+
+done   
 
 #    rm -f "$WATCH_FILE"
 #    while [ ! -e "$WATCH_FILE" ]; do
@@ -50,12 +59,3 @@ do
 #    done
 #    rm -f "$WATCH_FILE"
 #    
-#    # this has got to be my favorite command EVAR
-#    echo "Kill the search server... \ø/"
-#    killall -9 java
-#  
-#    # Start the main program
-#    cd $JETTY_HOME
-#    echo $PROG "${@:3}" 
-#    $PROG "${@:3}" &
-done   
