0db4aca79cabbea0aa8db0f60e27dc3dbcb939ec
Robert Kaye <rob@musicbrainz.org>
2016-10-30 17:51:39 +0000

    Pass the private IP to the search server in an env var

--- start-server.sh
+++ start-server.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
 #docker volume create --driver local --name search-indexer-data
 # later add: --detach
 HOSTNAME=`hostname`
+source /root/docker-server-configs/scripts/constants.sh
 docker run \
     --hostname $HOSTNAME \
     --restart unless-stopped \
@@ -12,4 +18,5 @@ docker run \
     --env SERVICE_8080_CHECK_TCP="true" \
     --env SERVICE_8080_CHECK_INTERVAL="15s" \
     --env SERVICE_8080_CHECK_TIMEOUT="3s" \
+    --env PRIVATE_IP=$PRIVATE_IP \
     searchserver_server
